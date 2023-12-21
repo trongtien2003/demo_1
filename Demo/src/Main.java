ec08c6ae81d90e843295550e540baa9d68a983f9
trongtien2003 <phanvotrongtien2003@gmail.com>
2023-12-21 19:25:06 +0700

    Change

---
+++ Demo/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+                System.out.println("Hello");
+                int a = 10;
+                int b = 1;
+                if (a > 0)
+                    System.out.println("Ket qua =" (a + b));
+    }
+}
